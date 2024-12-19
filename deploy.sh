6746b0a0c87d7b2a2c5840d0b14049981eb54b57
Kevin Shi <kevinxpshi@berkeley.edu>
2024-12-18 23:41:08 -0800

    cringe

--- deploy.sh
+++ deploy.sh
@@ -2,19 +2,11 @@
 
 set -e  # Exit immediately if a command exits with a non-zero status
 
-apt-get update && apt-get install -y \
-    libgstreamer-gl1.0-0 \
-    libgstreamer-plugins-bad1.0-0 \
-    libenchant-2-2 \
-    libsecret-1-0 \
-    libmanette-0.2-0 \
-    libgles2-mesa
-
 # Activate virtual environment
 VENV_DIR="venv"
 echo "Activating virtual environment..."
 source $VENV_DIR/bin/activate
-playwright install
+playwright install --with-deps
 
 # Run the Flask application
 echo "Running Flask application..."
