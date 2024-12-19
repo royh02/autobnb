9d6c2e7b79348fd694f6630cb36b48660e7b5d07
Kevin Shi <kevinxpshi@berkeley.edu>
2024-12-18 23:33:57 -0800

    try one more

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,14 @@
 
 set -e  # Exit immediately if a command exits with a non-zero status
 
+apt-get update && apt-get install -y \
+    libgstreamer-gl1.0-0 \
+    libgstreamer-plugins-bad1.0-0 \
+    libenchant-2-2 \
+    libsecret-1-0 \
+    libmanette-0.2-0 \
+    libgles2-mesa
+
 # Activate virtual environment
 VENV_DIR="venv"
 echo "Activating virtual environment..."
